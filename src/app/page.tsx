2934221c76d7b6474a69d39a022739840dabbd18
amirs18 <amir.shafat1@gmail.com>
2023-11-19 03:11:20 +0200

    added logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import GetLocationButton from '@components/GetLocationButton';
 import Login from '@components/Login';
 import { Metadata } from 'next';
-import { serverClient } from '@trpcProviders/serverClient';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/server/auth';
-import { redirect } from 'next/navigation';
+import Image from 'next/image';
 import AdminPanelButton from './_components/AdminPanelButton';
 
 export const metadata: Metadata = {
@@ -15,13 +14,14 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
     <main className=" mx-10 mt-5">
-      <h1 className="prose text-center text-4xl">
+      {/* <h1 className="prose text-center text-4xl">
         Welcome to{' '}
         <span className="prose text-4xl text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-green-500">
           Electric Rescue
         </span>
-      </h1>
+      </h1> */}
       <div className="flex flex-col items-center justify-center p-8">
+        <Image src="/logo.jpeg" alt="logo" width={400} height={100}></Image>
         <Login />
         <GetLocationButton />
         <AdminPanelButton />
